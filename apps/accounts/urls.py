947d2a9f5b8bc03e8bccc1ce5fa60b6c31af3ad4
Charles Eromose <Okuekhamhene@gmail.com>
2026-08-22 16:09:21 +0100

    updated urls.py

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -4,7 +4,11 @@ URL configuration for the accounts app.
 These URLs are included under /api/v1/auth/ via config/urls.py
 """
 from django.urls import path
+# Added for registration feature
 from .views import RegisterView
+# Added for login feature
+from .views import LoginView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     # path(route, view, name)
@@ -12,5 +16,9 @@ urlpatterns = [
     # view: The view class (must call .as_view() for CBVs)
     # name: A unique identifier for reverse URL lookups
     path('register/', RegisterView.as_view(), name='register'),
+     # Feature 2: Login (returns access + refresh tokens)
+    path('login/', LoginView.as_view(), name='login'),
+    # Feature 2: Token Refresh (returns new access token)
+    # We use SimpleJWT's built-in view with no customization for MVP.
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
-
