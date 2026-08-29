3ce1e42afde670613bcbc66c59a007d64ccd23e9
Charles Eromose <Okuekhamhene@gmail.com>
2026-08-29 15:12:21 +0100

    updated urls.py to create tasks

--- apps/todos/urls.py
+++ apps/todos/urls.py
@@ -1,5 +1,12 @@
+"""
+URL configuration for the todos app.
+
+Included under /api/v1/todos/ via config/urls.py
+"""
+
 from django.urls import path
+from .views import TaskCreateView
 
 urlpatterns = [
-    
+    path('', TaskCreateView.as_view(), name='task-create'),
 ]
